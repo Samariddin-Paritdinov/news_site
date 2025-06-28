04240403cd9d5073c3fbbf160be0b41f817913bd
Samariddin-Paritdinov <muzaffarovichsamarr@gmail.com>
2025-06-28 17:31:39 +0500

    dastlabki qilinishi kerak ishlar. startproject, startapp, secret keyni .env ga joylash va hokazo

---
+++ core/urls.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="eCommerce API",
+      default_version='v1',
+      description="eCommerce platform API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="admin@@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[],
+)
+
+
+urlpatterns = [
+
+
+
+    path('admin/', admin.site.urls),
+    # Swagger and Redoc URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        re_path(r'^rosetta/', include('rosetta.urls'))
+    ]
